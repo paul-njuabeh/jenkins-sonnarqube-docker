ea1b417d90ec1af5dd1875f168cb17122fb67a54
paul-njuabeh <117046940+paul-njuabeh@users.noreply.github.com>
2023-07-22 08:52:20 -0700

    Update servers.tf

--- servers.tf
+++ servers.tf
@@ -13,56 +13,56 @@ data "aws_ami" "latest-amazon-linux-image" {
 
 resource "aws_instance" "jenkins-server" {
   ami                         = "ami-0e820afa569e84cc1"
-  instance_type               = "t2.medium"
-  key_name                    = "project1-key"
+  instance_type               = var.instance_type_jenkins
+  key_name                    = var.key_name
   subnet_id                   = aws_subnet.myapp-subnet.id
   vpc_security_group_ids      = [aws_default_security_group.default-sg.id]
   availability_zone           = var.avail_zone_a
   associate_public_ip_address = true
   user_data                   = file("jenkins-script.sh")
   tags = {
-    Name = "${var.env_prefix}-server"
+    Name = "${var.env_prefix}-jenkins"
   }
 }
 
 resource "aws_instance" "docker-server" {
   ami                         = "ami-0e820afa569e84cc1"
-  instance_type               = "t2.medium"
-  key_name                    = "project1-key"
+  instance_type               = var.instance_type_docker
+  key_name                    = var.key_name
   subnet_id                   = aws_subnet.myapp-subnet.id
   vpc_security_group_ids      = [aws_default_security_group.default-sg.id]
   availability_zone           = var.avail_zone_a
   associate_public_ip_address = true
   user_data                   = file("docker-script.sh")
   tags = {
-    Name = "${var.env_prefix}-server"
+    Name = "${var.env_prefix}-docker"
   }
 }
 
 resource "aws_instance" "nexus-server" {
   ami                         = "ami-0e820afa569e84cc1"
-  instance_type               = "t2.medium"
-  key_name                    = "project1-key"
+  instance_type               = var.instance_type_nexus
+  key_name                    = var.key_name
   subnet_id                   = aws_subnet.myapp-subnet.id
   vpc_security_group_ids      = [aws_default_security_group.default-sg.id]
   availability_zone           = var.avail_zone_a
   associate_public_ip_address = true
   user_data                   = file("nexus-script.sh")
   tags = {
-    Name = "${var.env_prefix}-server"
+    Name = "${var.env_prefix}-nexus"
   }
 }
 
 resource "aws_instance" "sonarqube-server" {
   ami                         = "ami-0e820afa569e84cc1"
-  instance_type               = "t2.medium"
-  key_name                    = "project1-key"
+  instance_type               = var.instance_type_sonarqube
+  key_name                    = var.key_name
   subnet_id                   = aws_subnet.myapp-subnet.id
   vpc_security_group_ids      = [aws_default_security_group.default-sg.id]
   availability_zone           = var.avail_zone_a
   associate_public_ip_address = true
   user_data                   = file("sonarqube-script.sh")
   tags = {
-    Name = "${var.env_prefix}-server"
+    Name = "${var.env_prefix}-sonar"
   }
 }
